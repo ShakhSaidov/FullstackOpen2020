1bdd23d2230f95c42c9284dc198f968178a2e6ff
ShakhSaidov <www.blackbeard963@gmail.com>
2020-12-14 12:23:22 -0500

    Unicafe Exercise completed

--- Part1/Exercise 1.6 - 1.11 - Unicafe/src/index.js
+++ Part1/Exercise 1.6 - 1.11 - Unicafe/src/index.js
@@ -1,17 +1,90 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+const Title = ({ title }) => (
+  <h1> {title} </h1>
+)
+
+const Button = ({ onClick, text }) => (
+  <button onClick={onClick}> {text} </button>
+)
+
+const Statistic = ({text, value}) => {
+  if(text === "percentage"){
+    return(
+      <tr>
+        <td>{text}</td>
+        <td>{value}%</td>
+      </tr>
+    )
+  }
+
+  return (
+    <tr>
+        <td>{text}</td>
+        <td>{value}</td>
+      </tr>
+  )
+}
+
+const Statistics = ({good, neutral, bad, total, average, positivePercentage}) => {
+  if (total === 0) {
+    return (
+      <div>No feedback given</div>
+    )
+  }
+
+  return (
+    <div>
+      <table>
+        <tbody>
+          <Statistic text = "good" value = {good} />
+          <Statistic text = "neutral"  value = {neutral} />
+          <Statistic text = "bad"  value = {bad} />
+          <Statistic text = "total"  value = {total} />
+          <Statistic text = "average"  value = {average} />
+          <Statistic text = "percentage"  value = {positivePercentage} />
+        </tbody>
+      </table>
+    </div>
+  )
+
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const total = good + neutral + bad
+  const average = (good - bad) / total
+  const positivePercentage = good * 100 / total
+
+
+  const handleGoodClick = () => {
+    setGood(good + 1)
+  }
+
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1)
+  }
+
+  const handleBadClick = () => {
+    setBad(bad + 1)
+  }
+
+  return (
+    <div>
+      <Title title="Give Feedback" />
+      <Button onClick={handleGoodClick} text='Good' />
+      <Button onClick={handleNeutralClick} text='Neutral' />
+      <Button onClick={handleBadClick} text='Bad' />
+      <Title title="Statistics" />
+      <Statistics good={good} neutral={neutral} bad={bad} total={total} average={average} positivePercentage={positivePercentage}/>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />,
   document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
