1d4d9c9fe5364db6f515365df40bcf72449b0911
ShakhSaidov <www.blackbeard963@gmail.com>
2020-12-20 22:10:30 -0500

    Phonebook Exercise Completed

--- Part2/Exercises 2.6-2.12 - Phonebook/src/App.js
+++ Part2/Exercises 2.6-2.12 - Phonebook/src/App.js
@@ -1,26 +1,33 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import Search from './components/Search'
 import ContactForm from './components/ContactForm'
 import Contacts from './components/Contacts'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
-  const [ newName, setNewName ] = useState('')
-  const [ newNumber, setNewNumber] = useState('')
-  const [ newSearch, setNewSearch] = useState('')
+  const [persons, setPersons] = useState([])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [newSearch, setNewSearch] = useState('')
 
   const contacts = persons.filter(person =>
     person.name.toLowerCase().includes(newSearch.toLowerCase()))
 
+  const hook = () => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }
+  useEffect(hook, [])
+
   const addName = (e) => {
     e.preventDefault();
 
-    if(persons.some(person => person.name === newName)){
+    if (persons.some(person => person.name === newName)) {
       alert(`${newName} is already added to the phonebook!`)
     } else {
       const nameObject = {
@@ -34,29 +41,29 @@ const App = () => {
     }
   }
 
-  const handleSearchChange = (e) =>{
+  const handleSearchChange = (e) => {
     setNewSearch(e.target.value)
   }
 
-  const handleNameChange = (e) =>{
+  const handleNameChange = (e) => {
     setNewName(e.target.value)
   }
 
-  const handleNumberChange = (e) =>{
+  const handleNumberChange = (e) => {
     setNewNumber(e.target.value)
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <Search value={newSearch} handleSearchChange={handleSearchChange}/>
+      <Search value={newSearch} handleSearchChange={handleSearchChange} />
 
       <h3>Add Contact</h3>
       <ContactForm addName={addName} name={newName} nameChange={handleNameChange}
-        number={newNumber} numberChange={handleNumberChange}/>
+        number={newNumber} numberChange={handleNumberChange} />
 
       <h3>Contacts</h3>
-      <Contacts contacts={contacts}/>
+      <Contacts contacts={contacts} />
     </div>
   )
 }
