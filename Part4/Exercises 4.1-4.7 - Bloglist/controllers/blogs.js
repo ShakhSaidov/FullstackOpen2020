f53594230639601ad0a4d84e02a1ecc92ab911db
ShakhSaidov <www.blackbeard963@gmail.com>
2021-01-04 23:19:46 -0500

    Bloglist Backend created and refactored

---
+++ Part4/Exercises 4.1-4.7 - Bloglist/controllers/blogs.js
@@ -0,0 +1,35 @@
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blog')
+
+//GET request for blogs
+blogsRouter.get('/', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+})
+
+//GET request for phonebook
+blogsRouter.post('/', (request, response) => {
+  const body = request.body
+
+  if (body.title === undefined) {
+    return response.status(400).json({ error: 'content missing' })
+  }
+
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes
+  })
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+})
+
+module.exports = blogsRouter
