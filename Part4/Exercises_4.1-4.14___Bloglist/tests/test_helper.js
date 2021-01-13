156e809d59022cb9f4f3003a7a634b64501c89fc
ShakhSaidov <www.blackbeard963@gmail.com>
2021-01-12 21:31:28 -0500

    Bloglist: added users

--- Part4/Exercises_4.1-4.14___Bloglist/tests/test_helper.js
+++ Part4/Exercises_4.1-4.14___Bloglist/tests/test_helper.js
@@ -1,4 +1,5 @@
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 const testBlogs = [
   {
@@ -61,6 +62,13 @@ const blogsInDb = async () => {
   return blogs.map(blog => blog.toJSON())
 }
 
+const usersInDb = async () => {
+  const users = await User.find({})
+  return users.map(u => u.toJSON())
+}
+
 module.exports = {
-  testBlogs, blogsInDb
+  testBlogs,
+  blogsInDb,
+  usersInDb
 }
