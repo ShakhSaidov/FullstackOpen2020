293737e598da4e91fb31d8b8d3b7b1fd6268b3be
ShakhSaidov <www.blackbeard963@gmail.com>
2021-01-16 15:36:43 -0500

    build

---
+++ Part4/Exercises_4.1-4.22___Bloglist-Backend/build/precache-manifest.7b50d9b6a19b19a8e2ac8ec666913e6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6eee301bbc9580250f8fe7797d546b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "22b6a1d3e80a7e038f65",
+    "url": "/static/css/main.091f3c1a.chunk.css"
+  },
+  {
+    "revision": "8a5adb33992b54480e5e",
+    "url": "/static/js/2.2e15948b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.2e15948b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22b6a1d3e80a7e038f65",
+    "url": "/static/js/main.b29b5e3b.chunk.js"
+  },
+  {
+    "revision": "7eb8d6388ecf90523c80",
+    "url": "/static/js/runtime-main.7dc31f0f.js"
+  }
+]);
