894e493a15672baa7052e0730b8c2da6eff746bf
ShakhSaidov <www.blackbeard963@gmail.com>
2021-02-01 12:29:31 -0500

    build

--- Part4/Exercises_4.1-4.22___Bloglist-Backend/build/service-worker.js
+++ Part4/Exercises_4.1-4.22___Bloglist-Backend/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ebd6ff955ebbf40a3792dac51e6dbe29.js"
+  "/precache-manifest.8204b50b9fff5271df47d770e6726124.js"
 );
 
 self.addEventListener('message', (event) => {
