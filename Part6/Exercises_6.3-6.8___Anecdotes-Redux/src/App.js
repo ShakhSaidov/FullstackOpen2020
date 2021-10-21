9dc4447a531f71e7377e276b550012e285b19897
Shakhzod Saidov <saidovs@lafayette.edu>
2021-10-21 14:04:34 +0200

    completed 6.3

--- Part6/Exercises_6.3-6.8___Anecdotes-Redux/src/App.js
+++ Part6/Exercises_6.3-6.8___Anecdotes-Redux/src/App.js
@@ -1,13 +1,12 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
+import { voteUp } from './reducers/anecdoteReducer'
 
 const App = () => {
   const anecdotes = useSelector(state => state)
   const dispatch = useDispatch()
 
-  const vote = (id) => {
-    console.log('vote', id)
-  }
+  const vote = (id) => dispatch(voteUp(id))
 
   return (
     <div>
